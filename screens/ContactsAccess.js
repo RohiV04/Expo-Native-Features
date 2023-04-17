320d7d012a7d379607cebc0213ad360eb35432c9
Rohith <95613210+RohiV04@users.noreply.github.com>
2023-04-17 18:34:42 +0530

    Contacts

---
+++ screens/ContactsAccess.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View, Text, ScrollView } from "react-native";
+import * as Contacts from "expo-contacts";
+import { SafeAreaView } from "react-native-safe-area-context";
+export default function ContactsAccess() {
+  const [contact, setContact] = useState();
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [Email, setEmail] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Contacts.requestPermissionsAsync();
+      if (status === "granted") {
+        const { data } = await Contacts.getContactsAsync({
+          fields: [Contacts.Fields.Emails],
+        });
+
+        setContact(contact);
+        if (data.length > 0) {
+          const contact = data[0];
+          setContact(data);
+          console.log(data[0]);
+        }
+      }
+    })();
+  }, []);
+
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (contact) {
+    text = JSON.stringify(contact);
+  }
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View style={styles.container}>
+          <Text>Contacts Module Example</Text>
+          <Text>{text}</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
